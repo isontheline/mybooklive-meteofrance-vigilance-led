97728f033eb99ff3dc8eb857a9c8b5439e9c4fd8
Nono le petit robot <44212923+isontheline@users.noreply.github.com>
2019-05-25 22:17:12 +0200

    Update mfvigiled.sh

--- usr/local/sbin/mfvigiled.sh
+++ usr/local/sbin/mfvigiled.sh
@@ -3,8 +3,13 @@
 # Changer ici le département :
 DEPARTEMENT_A_VERIFIER="87"
 
+# Mode debug :
+DEBUG=0
+
 while :; do
-  echo "red" > /sys/class/leds/a3g_led/color
+  if [ $DEBUG -eq 1 ]; then
+    echo "Downloading vigilance..."
+  fi;
 
   curl \
   -s \
@@ -17,44 +22,64 @@ while :; do
   --retry-max-time 60 \
   -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" \
   http://vigilance.meteofrance.com/data/NXFR33_LFPW_.xml
-  
 
   # Pas de vigilance : coul="1"
   # Vigilance jaune : coul="2"
   # Vigilance orange : coul="3"
   # Vigilance rouge : coul="4"
-  COUL="1"
+  COUL="0"
   COULEUR_OK=0
   
-  if [ $COUL -eq "1" ]; then
-    echo "green" > /sys/class/leds/a3g_led/color
-    echo "no" > /sys/class/leds/a3g_led/blink
-    COULEUR_OK=1
-  fi
-  
-  if [ $COUL -eq "2" ]; then
-    echo "yellow" > /sys/class/leds/a3g_led/color
-    echo "no" > /sys/class/leds/a3g_led/blink
-    COULEUR_OK=1
-  fi
-  
-  if [ $COUL -eq "3" ]; then
-    echo "yellow" > /sys/class/leds/a3g_led/color
-    echo "yes" > /sys/class/leds/a3g_led/blink
-    COULEUR_OK=1
-  fi
-  
-  if [ $COUL -eq "4" ]; then
-    echo "red" > /sys/class/leds/a3g_led/color
-    echo "yes" > /sys/class/leds/a3g_led/blink
-    COULEUR_OK=1
-  fi
-  
-  if [ $COULEUR_OK -eq 0 ]; then
-    echo "blue" > /sys/class/leds/a3g_led/color
-    echo "yes" > /sys/class/leds/a3g_led/blink
-  fi
+  # Boucle de 5 minute :
+  for j in {1..5};
+    for j in {1..60};
+      if [ $COUL -eq "1" ]; then
+        echo "green" > /sys/class/leds/a3g_led/color
+        echo "no" > /sys/class/leds/a3g_led/blink
+        COULEUR_OK=1
+        
+        if [ $DEBUG -eq 1 ]; then
+          echo "Setting led color to green"
+        fi;
+      fi
 
-  # Attente de 10 minutes avant la prochaine actualisation
-  sleep 600
+      if [ $COUL -eq "2" ]; then
+        echo "yellow" > /sys/class/leds/a3g_led/color
+        echo "no" > /sys/class/leds/a3g_led/blink
+        COULEUR_OK=1
+        
+        if [ $DEBUG -eq 1 ]; then
+          echo "Setting led color to yellow"
+        fi;
+      fi
+
+      if [ $COUL -eq "3" ]; then
+        echo "yellow" > /sys/class/leds/a3g_led/color
+        echo "yes" > /sys/class/leds/a3g_led/blink
+        COULEUR_OK=1
+      fi
+
+      if [ $COUL -eq "4" ]; then
+        echo "red" > /sys/class/leds/a3g_led/color
+        echo "yes" > /sys/class/leds/a3g_led/blink
+        COULEUR_OK=1
+        
+        if [ $DEBUG -eq 1 ]; then
+          echo "Setting led color to red"
+        fi;
+      fi
+
+      if [ $COULEUR_OK -eq 0 ]; then
+        echo "blue" > /sys/class/leds/a3g_led/color
+        echo "yes" > /sys/class/leds/a3g_led/blink
+        
+        if [ $DEBUG -eq 1 ]; then
+          echo "Setting led color to blue"
+        fi;
+      fi
+
+      # Attente d'une seconde avant actualisation de la led à nouveau
+      sleep 1
+    done
+  done
 done
